e0a7aa9ccb1fc2e2bfc776d49806c73ec561acf4
kirill <kouznetsov2007@gmail.com>
2023-07-18 20:58:56 +0300

    commit2

--- test1.ps1
+++ test1.ps1
@@ -1,1 +1,14 @@
-Write-Host "hello world"
+
+Write-Host "----" -ForegroundColor Green
+
+
+$items=Get-ChildItem -Path "D:\*.txt" -Recurse  
+
+foreach ($i in $items){
+    Write-Host $i.FullName
+
+    
+    Break
+}
+
+#Write-Host "hello world"
