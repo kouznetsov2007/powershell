bf01a18d4792458a606d47ad843ea069a73b657b
kirill <kouznetsov2007@gmail.com>
2023-07-19 19:45:37 +0300

    success. colour lines

--- test1.ps1
+++ test1.ps1
@@ -2,13 +2,28 @@
 Write-Host "----" -ForegroundColor Green
 
 
-$items=Get-Content -Path "D:\*.txt" -ReadCount
+$items=Get-ChildItem -Path "D:\video\Kino_kollekciya\Foto.txt" -Recurse
 
 foreach ($i in $items){
-    Write-Host $i.FullName
-
+    Write-Host $i.FullName -ForegroundColor DarkBlue
+    $contents=Get-Content -Path $i.FullName
+    $s=$true
+    foreach($c in $contents){ 
+       if($c.Length -eq 0) {
+        continue
+       }
+        if ($s) {
+            Write-Host $c -ForegroundColor DarkYellow
+            $s=$false 
+            
     
-    Break
+        }else{
+            Write-Host $c -ForegroundColor Red
+            $s=$true
+        }
+    }
+    
+    
 }
 
 #Write-Host "hello world"
