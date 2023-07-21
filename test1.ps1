45c39a1aa8ac87f9b61885877ef129dfe40007ea
kirill <kouznetsov2007@gmail.com>
2023-07-21 17:05:12 +0300

    repalcemnts

--- test1.ps1
+++ test1.ps1
@@ -2,24 +2,21 @@
 Write-Host "----" -ForegroundColor Green
 
 
-$items=Get-ChildItem -Path "D:\video\Kino_kollekciya\Foto.txt" -Recurse
+$items=Get-ChildItem -Path "D:\test.txt" -Recurse
+
+$find="kuku"
 
 foreach ($i in $items){
     Write-Host $i.FullName -ForegroundColor DarkBlue
     $contents=Get-Content -Path $i.FullName
-    $s=$true
+    
     foreach($c in $contents){ 
        if($c.Length -eq 0) {
         continue
-       }
-        if ($s) {
-            Write-Host $c -ForegroundColor DarkYellow
-            $s=$false 
-            
-    
-        }else{
-            Write-Host $c -ForegroundColor Red
-            $s=$true
+       } 
+       
+       if($c.Contains($find)){
+        $c -replace $find , 'poka'
         }
     }
     
